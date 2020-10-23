ed32011b92332001d8974019b48cc7e23f11a7fb
ZeeLog <Danjel.Zammit@gmail.com>
2020-10-23 20:48:20 +0200

    mido: Build btconfigstore 2.0 from source

--- device.mk
+++ device.mk
@@ -131,7 +131,8 @@ PRODUCT_PACKAGES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.btconfigstore@1.0.vendor
+    vendor.qti.hardware.btconfigstore@1.0.vendor \
+    vendor.qti.hardware.btconfigstore@2.0.vendor
 
 # Camera
 PRODUCT_PACKAGES += \
