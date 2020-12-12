36e1651c295ecba66af9b73075334dadff43a242
Gagan Malvi <malvigagan@gmail.com>
2020-12-12 13:00:36 +0530

    mido: CesiumOS?
    * note: this is not me being a buildbot, i just don't trust builds from other devs
    
    Signed-off-by: Gagan Malvi <malvigagan@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_mido.mk
+    $(LOCAL_DIR)/cesium_mido.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_mido-user \
-    arrow_mido-userdebug \
-    arrow_mido-eng
+    cesium_mido-user \
+    cesium_mido-userdebug \
+    cesium_mido-eng
