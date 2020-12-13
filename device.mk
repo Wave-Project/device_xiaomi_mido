be7f20b2ee7d2c5cc06bb2a7b7a41252eb592d71
Gagan Malvi <malvigagan@gmail.com>
2020-12-13 18:41:51 +0530

    mido: Drop DeviceParts
    * Looks, and works gay.
    
    Signed-off-by: Gagan Malvi <malvigagan@gmail.com>

--- device.mk
+++ device.mk
@@ -168,10 +168,6 @@ PRODUCT_PACKAGES += \
     libgenlock \
     libtinyxml
 
-# Device-specific settings
-PRODUCT_PACKAGES += \
-	DeviceParts
-
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
