f738612662ee13dc8da8f7cf466cfd5f973c0cae
Gagan Malvi <malvigagan@gmail.com>
2020-12-13 19:33:28 +0530

    mido: Add up trim properties from mido-user-7.0-NRD90M-9.7.4-release-keys
    Signed-off-by: Gagan Malvi <malvigagan@gmail.com>

--- configs/properties/vendor_prop.mk
+++ configs/properties/vendor_prop.mk
@@ -131,9 +131,16 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	ro.vendor.qti.sys.fw.bservice_enable=true \
 	ro.vendor.qti.sys.fw.bservice_age=5000 \
 	ro.vendor.qti.sys.fw.bservice_limit=5 \
-	ro.vendor.qti.sys.fw.empty_app_percent=50 \
 	ro.vendor.qti.am.reschedule_service=true \
 	vendor.perf.gestureflingboost.enable=true
+
+# Trim Properties - from mido-user-7.0-NRD90M-9.7.4-release-keys
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.sys.fw.use_trim_settings=true \
+	ro.sys.fw.empty_app_percent=50 \
+	ro.sys.fw.trim_empty_percent=100 \
+	ro.sys.fw.trim_cache_percent=100 \
+	ro.sys.fw.trim_enable_memory=2147483648
 
 # Netmgrd
 PRODUCT_PROPERTY_OVERRIDES += \
